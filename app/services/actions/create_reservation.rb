a8cd905cf23e07e2f29622fd4deb5c9574606274
Pawel Licznar <pawellicznar@gmail.com>
2019-07-01 09:43:18 +0200

    add create reservation action

---
+++ app/services/actions/create_reservation.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module Actions
+  class CreateReservation
+    def initialize(user:, event_id:, tickets_quantity:)
+      @user             = user
+      @event_id         = event_id
+      @tickets_quantity = tickets_quantity
+    end
+
+    def call
+      return validation_error_response unless validation_errors.empty?
+      return Actions::Response::Success.new if create_reservation
+      Actions::Response::Error.new(result: { errors: 'Error occured' })
+    end
+
+    private
+
+    attr_reader :user, :event_id, :tickets_quantity, :reservation
+
+    def create_reservation
+      ActiveRecord::Base.transaction do
+        return true if create_reservation_record && assaign_tickets_to_reservation
+        raise ActiveRecord::Rollback
+      end
+    end
+
+    def validation_errors
+      @validation_errors ||= ::Validators::ReservationTicketsQuantity
+                             .new(tickets_quantity, available_tickets)
+                             .validate
+    end
+
+    def create_reservation_record
+      @reservation = Reservation.create(event_id: event_id, user: user)
+    end
+
+    def assaign_tickets_to_reservation
+      tickets_quantity.each do |type, quantity|
+        Ticket
+          .where(
+            ticket_type: Ticket::TICKET_TYPES.fetch(type),
+            event_id:    event_id
+          )
+          .limit(quantity)
+          .update(reservation_id: reservation.id)
+      end
+    end
+
+    def validation_error_response
+      Actions::Response::Error.new(
+        result: {
+          errors: validation_errors.join('. ')
+        }
+      )
+    end
+
+    # fetch data just before validate and create reservation
+    # to ensure that we operate on updated values
+    def available_tickets
+      tickets = ::Repository::EventAvailableTickets.call(event_id)
+      tickets.group_by { |ticket_hash| ticket_hash['type'] }
+    end
+  end
+end
---
+++ spec/services/actions/create_reservation_spec.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Actions::CreateReservation do
+  let(:user)            { create :user }
+  let(:event)           { create :event }
+  let(:event_2)         { create :event }
+  let(:ordered_tickets) { { avoid_one: 1 } }
+  let(:instance) { described_class.new(user: user, event_id: event.id, tickets_quantity: ordered_tickets) }
+
+  describe 'call' do
+    before do
+      create_list :ticket, 3, event: event, ticket_type: 'avoid_one'
+      create_list :ticket, 4, event: event_2, ticket_type: 'avoid_one'
+      create_list :ticket, 2, event: event, ticket_type: 'even'
+
+      validator = instance_double('Validators::ReservationTicketsQuantity')
+      allow(Validators::ReservationTicketsQuantity).to receive(:new).and_return(validator)
+      allow(validator).to receive(:validate).and_return(errors_array)
+    end
+
+    context 'action successful' do
+      let(:errors_array) { [] }
+
+      it 'creates new reservation with proper event and assaign ticket' do
+        instance.call
+        reservation = user.reservations.first
+
+        expect(user.reservations.count).to eq 1
+        expect(reservation.event_id).to eq event.id
+        expect(reservation.tickets.count).to eq 1
+      end
+
+      it 'validates quantity of ordered tickets' do
+        validator = instance_double('Validators::ReservationTicketsQuantity')
+        expect(Validators::ReservationTicketsQuantity)
+          .to receive(:new)
+          .with(ordered_tickets, kind_of(Hash))
+          .and_return(validator)
+        expect(validator).to receive(:validate).and_return(errors_array)
+
+        instance.call
+      end
+
+      it 'returns success response object' do
+        expect(instance.call).to be_instance_of(Actions::Response::Success)
+      end
+    end
+
+    context 'action failed' do
+      context 'validation failed' do
+        let(:errors_array) { ['realy_bad_error'] }
+
+        it 'returns error response object' do
+          expect(instance.call).to be_instance_of(Actions::Response::Error)
+        end
+      end
+
+      context 'transaction failed' do
+        shared_examples_for 'rollbacked_transaction' do
+          let(:errors_array) { [] }
+
+          it "dosen't update tickets reservation" do
+            expect(user.reservations.count).to eq 0
+            expect(event.tickets.where(reservation_id: nil).count).to eq 5 # all event tickets
+          end
+
+          it 'returns error response object' do
+            expect(instance.call).to be_instance_of(Actions::Response::Error)
+          end
+        end
+
+        context 'reservation create failed' do
+          before do
+            allow(Reservation).to receive(:create).and_return(false)
+            instance.call
+          end
+
+          it_behaves_like 'rollbacked_transaction'
+        end
+
+        context 'tickets update raise error' do
+          before do
+            allow(Ticket).to receive(:where).and_raise(ActiveRecord::Rollback)
+            instance.call
+          end
+
+          it_behaves_like 'rollbacked_transaction'
+        end
+      end
+    end
+  end
+end
